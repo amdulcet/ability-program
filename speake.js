1b67350a3a4fe196ad7706fadb158863ec0bc52d
amdulcet <amdulcet@gmail.com>
2024-06-23 18:16:41 +0200

    fff

--- speake.js
+++ speake.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     } else if (currentPageUrl.includes('contact')) {
       utteranceText = ' You are now on the contact page. Come to it whenever you feel like you want to send a message to the Ability owners. We are here to help you with any further modifications and assistance to make your program the best for your learning. Here is a quick guide to contact us: Press the space bar button on your keyboard and speak the name of your e-mail. Press the space bar button for the second time if you are ready to speak your message, speak your message. Thirdly, press the space bar again and give a confirmation response just like "yes". With those few steps, your message will reach us directly. Thank you.';
     } else if (currentPageUrl.includes('youtube')) {
-      utteranceText = 'You are now on the YouTube page. Press any key different from the space bar key and speak the name of the song you want to listen to.';
+      utteranceText = 'You are now on the YouTube page. Press any key different from the space bar key and speak what you want to listen to.';
     } else if (currentPageUrl.includes('library')) {
       utteranceText = 'You are now on the library page. Press any key different from the space bar key and give a command of what you want to do with the library.';
     } else if (currentPageUrl.includes('guide')) {
